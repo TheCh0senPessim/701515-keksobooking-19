0bf85464ec6ab7589969fa6e20569b7ac7d96388
Никита <klosr@list.ru>
2020-02-23 16:08:51 +0300

    Черновой вариант

---
+++ js/main.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+var mocks = [];
+var mocksAmount = 8;
+var avatarCount = 0;
+
+var getRandom = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var getRandomIndex = function (arr) {
+  return Math.floor(Math.random() * (arr.length - 1 + 1))
+};
+
+var exampleMock = {
+  author: {
+    avatar: 'img/avatars/user0' + avatarCount++ + '.png',
+  },
+
+  offer: {
+    title: 'some title',
+    address: '600, 350',
+    price: 500,
+    type: ['palace', 'flat', 'house', 'bungalo'],
+    rooms: [1, 2, 3, 4],
+    guests: [1, 2, 3, 4],
+    checkin: ['12:00', '13:00', '14:00'],
+    checkout: ['12:00', '13:00', '14:00'],
+    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+    description: 'some decription',
+    photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+  },
+  location: {
+    x: getRandom(0, 1150),
+    y: getRandom(130, 630)
+  }
+};
+
+var getMock = function (exampleOBJ) {
+  var someObj = {};
+  someObj.author = exampleOBJ.author;
+  someObj.author.avatar = 'img/avatars/user0' + getRandom(1, 8) + '.png';
+
+  someObj.offer = exampleOBJ.offer;
+
+  someObj.location = exampleOBJ.location;
+  someObj.location.x = exampleOBJ.location.x;
+  someObj.location.y = exampleOBJ.location.y;
+};
+
+// console.log(getMock(exampleMock));
+
+//функция создания массива объектов
+
+var createMocks = function (arr) {
+  for (var i = 0; i <= mocksAmount - 1; i++) {
+    var mock = getMock(exampleMock);
+    arr.push(mock);
+  }
+  return arr;
+};
+
+//записываем в массив результат выполнения функции
+mocks = createMocks(mocks);
+
+var mapPins = map.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+//функцися создания DOM-объекта
+var renderPin = function (element) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style = 'left: ' + getRandom(0, 1150) + 'px; top: ' + getRandom(130, 630) + 'px;';
+  pinElement.querySelector('img').src = 'img/avatars/user0' + avatarCount++ + '.png';
+  pinElement.querySelector('img').alt = 'заголовок объявления';
+  return pinElement;
+};
+
+//создаем фрагмент
+var renderElements = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i <= mocks.length - 1; i++) {
+    fragment.appendChild(renderPin(mocks[i]));
+  }
+  mapPins.appendChild(fragment);
+};
+
+renderElements();
