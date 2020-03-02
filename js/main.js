17775fc09db8876b693286a4101f3784cdb9f64e
Никита <klosr@list.ru>
2020-03-03 00:06:10 +0300

    рефакторинг

--- js/main.js
+++ js/main.js
@@ -5,73 +5,67 @@ map.classList.remove('map--faded');
 
 var mocks = [];
 var mocksAmount = 8;
+var avatarCount = 0;
+var mapPins = map.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var exampleMock = {
-  avatarCount: 0,
-  offer: {
+var getMockAvatar = function () {
+  return {
+    avatar: 'img/avatars/user0' + ++avatarCount + '.png'
+  };
+};
+
+var getMockOffer = function () {
+  return {
     title: 'some title',
     address: '600, 350',
-    price: 500,
-    type: ['palace', 'flat', 'house', 'bungalo'],
-    rooms: [1, 2, 3, 4],
-    guests: [1, 2, 3, 4],
-    checkin: ['12:00', '13:00', '14:00'],
-    checkout: ['12:00', '13:00', '14:00'],
-    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-    description: 'some decription',
-    photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-  },
-  location: {
+    price: getRandom(500, 5000)
+  };
+};
+
+var getMockLocation = function () {
+  return {
     x: getRandom(0, 1150),
     y: getRandom(130, 630)
-  },
-
-  getMockAvatar: function () {
-    return {
-      avatar: 'img/avatars/user0' + ++this.avatarCount + '.png'
-    };
-  },
-
-  getMockOffer: function () {
-    return {
-      title: this.title,
-      address: this.address,
-      price: this.price
-    };
-  },
-
-  getMockLocation: function () {
-    return {
-      x: getRandom(0, 1150),
-      y: getRandom(130, 630)
-    };
-  },
-
-  getMock: function () {
-    return {
-      author: this.getMockAvatar(),
-      offer: this.getMockOffer(),
-      location: this.getMockLocation()
-    };
-  }
+  };
 };
 
-var createMocks = function (arr) {
+var getMock = function () {
+  return {
+    author: getMockAvatar(),
+    offer: getMockOffer(),
+    location: getMockLocation()
+  };
+};
+
+// var exampleMock = {
+//   offer: {
+//     type: ['palace', 'flat', 'house', 'bungalo'],
+//     rooms: [1, 2, 3, 4],
+//     guests: [1, 2, 3, 4],
+//     checkin: ['12:00', '13:00', '14:00'],
+//     checkout: ['12:00', '13:00', '14:00'],
+//     features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+//     description: 'some decription',
+//     photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+//   }
+// };
+
+var createMocks = function () {
+  var mocksArr = [];
   for (var i = 0; i <= mocksAmount - 1; i++) {
-    var mock = exampleMock.getMock();
-    arr.push(mock);
+    var mock = getMock();
+    mocksArr.push(mock);
   }
-  return arr;
+  return mocksArr;
 };
 
-mocks = createMocks(mocks);
-
-var mapPins = map.querySelector('.map__pins');
-var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+mocks = createMocks();
+// console.log(mocks);
 
 var renderPin = function (mock) {
   var pinElement = pinTemplate.cloneNode(true);
@@ -83,8 +77,8 @@ var renderPin = function (mock) {
 
 var renderElements = function () {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i <= mocks.length - 1; i++) {
-    fragment.appendChild(renderPin(mocks[i]));
+  for (var j = 0; j <= mocks.length - 1; j++) {
+    fragment.appendChild(renderPin(mocks[j]));
   }
   mapPins.appendChild(fragment);
 };
