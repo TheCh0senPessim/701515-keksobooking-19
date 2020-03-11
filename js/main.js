edf78bed5946f393437750ae43a5ee9dbc3fa85e
Никита <klosr@list.ru>
2020-03-11 15:59:56 +0300

    реализовал перемещение главной метки и заполнение поля адреса по координатам

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ var ENTER_KEY = 'Enter';
 
 var MAIN_PIN_WIDTH = 62;
 var MAIN_PIN_HEIGHT = 84;
-var map = document.querySelector('.map');
-// map.classList.remove('map--faded');
 
+var map = document.querySelector('.map');
 var mocks = [];
 var mocksAmount = 8;
 var avatarCount = 0;
@@ -129,19 +128,31 @@ var renderElements = function () {
   mapPins.appendChild(fragment);
 };
 
-var renderCards = function () {
-  var fragmentCard = document.createDocumentFragment();
-  for (var i = 0; i <= mocks.length - 1; i++) {
-    fragmentCard.appendChild(renderCard(mocks[i]));
-  }
-  map.insertBefore(fragmentCard, map.querySelector('.map__filters-container'));
-};
+// var renderCards = function () {
+//   var fragmentCard = document.createDocumentFragment();
+//   for (var i = 0; i <= mocks.length - 1; i++) {
+//     fragmentCard.appendChild(renderCard(mocks[i]));
+//   }
+//   map.insertBefore(fragmentCard, map.querySelector('.map__filters-container'));
+// };
 
-// renderElements();
-renderCards();
+// Вернуться к карточкам во второй части задания
+// renderCards();
 
-var mainPin = document.querySelector('.map__pin--main');
 var adForm = document.querySelector('.ad-form');
+
+// Пробую описать изменение одной функцией:
+// var changeAvailability = function (elements, isEnable) {
+//   console.log(isEnable);
+//   for (var i = 0; i <= elements.length; i++) {
+//     console.log(elements[i]);
+//     elements[i].disabled = isEnable;
+//   }
+// };
+// Но не работает
+// changeAvailability(mapFiltersControls, true);
+// changeAvailability(adForm.children, false);
+
 var formControls = adForm.children;
 for (var i = 0; i <= formControls.length - 1; i++) {
   formControls[i].disabled = true;
@@ -153,16 +164,25 @@ for (var i = 0; i <= mapFiltersControls.length - 1; i++) {
   mapFiltersControls[i].disabled = true;
 }
 
+var mainPin = document.querySelector('.map__pin--main');
 var adressInput = adForm.querySelector('#address');
-adressInput.value = parseInt(mainPin.style.left, 10) + ', ' + parseInt(mainPin.style.top, 10);
-console.log(mainPin.style);
+
+var getMainPinPosition = function () {
+  var mainX = parseInt(mainPin.style.left, 10) + MAIN_PIN_WIDTH / 2;
+  var mainY = parseInt(mainPin.style.top, 10) + MAIN_PIN_HEIGHT;
+  adressInput.value = mainX + ', ' + mainY;
+};
+
+getMainPinPosition();
 
 var onMainPinClick = function () {
   map.classList.remove('map--faded');
   adForm.classList.remove('ad-form--disabled');
-  var formControls = adForm.children;
   for (var i = 0; i <= formControls.length - 1; i++) {
     formControls[i].disabled = false;
+  }
+  for (var j = 0; j <= mapFiltersControls.length - 1; j++) {
+    mapFiltersControls[j].disabled = false;
   }
   renderElements();
 };
@@ -170,6 +190,35 @@ var onMainPinClick = function () {
 mainPin.addEventListener('mousedown', function (evt) {
   if (evt.button === 0) {
     onMainPinClick();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    getMainPinPosition();
   }
 });
 
